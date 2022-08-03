83e1385920f4ab8ac076048e605fe3991eb286dd
John Kibirige <kibirigejohn005@gmail.com>
2022-08-03 20:26:29 +0300

    Fix bugs for mainly the Microsoft edge browser

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/**
+ *Constructor function for making book objects
+ * @param {string} title
+ * @param {string} author
+ * @param {Number} id
+ */
 function Book(title, author, id = Date.now()) {
   this.title = title;
   this.author = author;
@@ -15,7 +21,7 @@ const form = document.querySelector('#book-form');
  */
 function storeToLocal(array) {
   array = JSON.stringify(array);
-  localStorage.setItem('books', array);
+  window.localStorage.setItem('books', array);
 }
 
 /**
@@ -24,81 +30,80 @@ function storeToLocal(array) {
    * @returns {array}
    */
 function retrieveFromStorage() {
-  return JSON.parse(localStorage.getItem('books'));
-}
-/**
- * This method creates a container of elements
- * that are appended to the book list element
- * @param {string} title
- * @param {string} author
- * @param {number} bookId
- */
-function populateUi(title, author, bookId) {
-  const titleAndAuthor = document.createElement('p');
-  titleAndAuthor.classList.add('title-author');
-  titleAndAuthor.innerText = `"${title}" by ${author}`;
-  const removeBtn = document.createElement('button');
-  removeBtn.innerText = 'Remove';
-
-  // create container and append all these
-  const bookContainer = document.createElement('div');
-  bookContainer.classList.add('book-container');
-
-  bookContainer.appendChild(titleAndAuthor);
-  bookContainer.appendChild(removeBtn);
-
-  bookList.appendChild(bookContainer);
-
-  removeBtn.addEventListener('click', () => {
-    bookList.removeChild(bookContainer);
-
-    // we update the local storage as well
-    let fromLocal = retrieveFromStorage();
-    fromLocal = fromLocal.filter((buk) => buk.id !== bookId);
-    storeToLocal(fromLocal);
-  });
+  return JSON.parse(window.localStorage.getItem('books'));
 }
 
 /**
  * Class used to define a single book-list instance
  */
 class BookList {
-  constructor() {
-    this.list = [];
-  }
-
   /**
    * This method adds a book object to the book list
    * @param {string} title
    * @param {string} author
    */
-  addBook(title, author) {
+  static addBook(title, author) {
     const book = new Book(title, author);
-    this.list.push(book);
+    // render book to ui
+    this.populateUi(title, author, book.id);
+
+    // make the necessary updates in the ui
     const fromLocalStorage = retrieveFromStorage();
-    storeToLocal([...fromLocalStorage, book]);
+    if (fromLocalStorage !== null) {
+      fromLocalStorage.push(book);
+      storeToLocal(fromLocalStorage);
+    } else {
+      storeToLocal([book]);
+    }
   }
 
   /**
-   * This method appends only the latest added object
-   * added to the UI
+   * This method removes an object with the id given from the local storage
+   * @param {Number} id
    */
-  render() {
-    const len = this.list.length;
-    const latestAddition = this.list[len - 1];
-    populateUi(latestAddition.title, latestAddition.author, latestAddition.id);
+  static removeBook(id) {
+    let fromLocal = retrieveFromStorage();
+    fromLocal = fromLocal.filter((buk) => buk.id !== id);
+    storeToLocal(fromLocal);
+  }
+
+  /**
+   * This method creates a container of elements
+   * that are appended to the book list element
+   * @param {string} title
+   * @param {string} author
+   * @param {number} bookId
+   */
+  static populateUi(title, author, bookId) {
+    const titleAndAuthor = document.createElement('p');
+    titleAndAuthor.classList.add('title-author');
+    titleAndAuthor.innerText = `"${title}" by ${author}`;
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = 'Remove';
+
+    // create container and append all these
+    const bookContainer = document.createElement('div');
+    bookContainer.classList.add('book-container');
+
+    bookContainer.appendChild(titleAndAuthor);
+    bookContainer.appendChild(removeBtn);
+
+    bookList.appendChild(bookContainer);
+
+    removeBtn.addEventListener('click', () => {
+      bookList.removeChild(bookContainer);
+      this.removeBook(bookId);
+    });
   }
 }
 
+// Carry out all dynamic operations only when the window has loaded
 window.addEventListener('load', () => {
-  // Create an instance of the book list class
-  const bookListInstance = new BookList();
-
   // render all books stored in the local storage
   const retrieved = retrieveFromStorage();
   if (retrieved !== null) {
     retrieved.forEach((book) => {
-      populateUi(book.title, book.author, book.id);
+      BookList.populateUi(book.title, book.author, book.id);
     });
   }
 
@@ -109,8 +114,6 @@ window.addEventListener('load', () => {
     // received all user inputs
     const titleReceived = document.getElementById('title').value;
     const authorReceived = document.getElementById('author').value;
-
-    bookListInstance.addBook(titleReceived, authorReceived);
-    bookListInstance.render();
+    BookList.addBook(titleReceived, authorReceived);
   });
 });
